951f457555cfeae8b5796d98d3d1da2db3044eea
kosprok <kosprolv@gmail.com>
2021-01-25 00:58:32 +0200

    Sample code

--- js/player.js
+++ js/player.js
@@ -1,8 +1,8 @@
-function toggle(){
-    const trailer= document.querySelector(".trailer")
-    // trailer.classList.toggle("active")
-    const video= document.querySelector("video")
+function toggle() {
+    const trailer = document.querySelector(".trailer");
+    const video = document.querySelector("video");
     trailer.classList.toggle("active");
     video.pause();
     video.currentTime = 0;
 }
+  
