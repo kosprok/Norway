68edf254563d5314439909fd262ae18540757e7f
kosprok <kosprolv@gmail.com>
2021-01-25 16:54:04 +0200

    Sample code

--- js/custom.js
+++ js/custom.js
@@ -2,9 +2,29 @@ $(function() {
     $('.responsive').slick({
         dots: true,
         infinite: true,
+        speed: 300,
         slidesToShow: 3,
         slidesToScroll: 1,
-        autoPlay: true,
-        autoPlaySpeed: 3000
-    });
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
 });
