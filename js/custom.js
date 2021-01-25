e3e7697ba9fb9315e75865c381d574b0518c05ea
kosprok <kosprolv@gmail.com>
2021-01-25 16:29:03 +0200

    Sample code

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.responsive').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoPlay: true,
+        autoPlaySpeed: 3000
+    });
+});
